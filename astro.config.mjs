f0457d451bf4e525d565c6f71ab5b2b953e42608
Gabriel Balbontin <bagxxi@gmail.com>
2024-03-11 21:08:57 -0300

    primera carga de archivos para deploy

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), svelte(), react()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+    },
+    imageService: true,
+    isr: true,
+    functionPerRoute: true,
+  }),
+});
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+
+    let greeting = ""
+
+    if (currentHour < 12 && currentHour > 5) {
+        greeting = "Buen día, viteh"
+    } else if (currentHour > 12 && currentHour < 20 ) {
+        greeting = "Buenas tardes, viteh"
+    } else {
+        greeting = "Buenas noches, viteh"
+    }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
