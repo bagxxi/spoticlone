d7d6db0baf38006844a8f8bd3e00b1724b3fb39a
Gabriel Balbontin <bagxxi@gmail.com>
2024-03-11 23:17:19 -0300

    carga para netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    imagesConfig: {
-      sizes: [320, 640, 1280],
-    },
-    imageService: true,
-    isr: true,
-    functionPerRoute: true,
-  }),
+  adapter: netlify()
 });
