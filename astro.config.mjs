89aa1582275e29afadf0f689491c1d2b36bc2ecf
Gabriel Balbontin <bagxxi@gmail.com>
2024-03-17 14:14:53 -0300

    commit para deploy en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
-  adapter: netlify()
+  adapter: vercel()
 });
